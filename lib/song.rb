2848703957f8578ffcf0cdf919c66c1d0119ab5e
Christine Zosche <christine.zosche@gmail.com>
2020-05-04 18:24:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,43 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name == name
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+      if self.find_by_name(name)
+        self.find_by_name(name)
+      else
+        self.create_by_name(name)
+      end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+  
+  def self.new_from_filename
+    
 end
